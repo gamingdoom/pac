8581ee30547137d206f852ec6f72d576c74fee3c
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2021-11-04 12:56:14 -0700

    Create pac.sh

---
+++ pac.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+args1=`echo $1 | cut -c1-2`
+pacpass=`echo $* | cut -c1-1,3-`
+uid=`id -u`
+
+if (($uid != 0)); then
+    echo "Please run with root"
+    exit 2
+fi
+
+case $args1 in
+    -P*)
+        if [[ $pacpass == - ]] 
+        then
+            echo "Please use a pacman argument after -P. ex -PSy"
+            exit 1
+        else
+        echo $pacpass
+        pacman $pacpass
+        fi
+    ;;
+    -A)
+        
+    ;;
+    *)
+        echo 'No argument provided'
+    ;;
+esac
